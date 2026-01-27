babf93499daa7340d89a40b6283fd014b75b506e
Habiba Sultana <habiba@tamu.edu>
2026-01-27 00:21:17 -0600

    First answer

---
+++ Lab_2/lab_2_python_script.py
@@ -0,0 +1,6 @@
+#First Question: Take the following list and multiply all list items together.
+part1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+result1 = 1
+for i in part1:
+    result1 = result1 * i
+print ( "the answer to question 1 is: ", result1)
